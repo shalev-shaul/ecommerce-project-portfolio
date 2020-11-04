e239454d70f7ada52b7fcc87822f7bf1a2fc5dfb
unknown <sshaul2704@gmail.com>
2020-11-04 12:30:51 +0200

    changes

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -6,7 +6,13 @@ import {
   productListReducer,
 } from "./reducers/productReducers";
 
-const initialState = {};
+const initialState = {
+  cart: {
+    cartItems: localStorage.getItem("cartItems")
+      ? JSON.parse(localStorage.getItem("cartItems"))
+      : [],
+  },
+};
 const reducer = combineReducers({
   productList: productListReducer,
   productDetails: productDetailsReducer,
