4e6c6350bf35826cd00ba28f54fbaadcc249e518
Shalev <sshaul2704@gmail.com>
2020-11-04 12:15:16 +0200

    changes

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,5 +1,6 @@
 import { applyMiddleware, combineReducers, compose, createStore } from "redux";
 import thunk from "redux-thunk";
+import { cartReducer } from "./reducers/cartReducers";
 import {
   productDetailsReducer,
   productListReducer,
@@ -9,6 +10,7 @@ const initialState = {};
 const reducer = combineReducers({
   productList: productListReducer,
   productDetails: productDetailsReducer,
+  cart: cartReducer,
 });
 const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 const store = createStore(
